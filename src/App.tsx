bd008ab4c45b54d53ff2e646312c1eeb6d0d4d23
Sabina Mejlisson <sabinamejlisson@gmail.com>
2025-02-17 09:49:55 +0100

    Added List.tsx and list.scss

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,29 @@ import Card from "./Components/Card/Card";
 import Button from "./Components/Button/Button";
 import Student from "./Components/Students/Student";
 import UserGreeting from "./Components/UserGreeting/UserGreeting";
+import List from "./Components/List/List";
 
 
 
 function App () {
-    return(
-      <>
-        <Card/>
-        <Card/>
+  const fruits = [{id: 1, name:'apple', calories:95}, 
+                  {id: 2, name:'banana', calories: 45}, 
+                  {id: 3, name:'coconut', calories: 50}, 
+                  {id: 4, name:'pineapple', calories: 277}, 
+                  {id: 5, name:'elderberry', calories: 73},];
+                  
+  const vegetables = [{id: 6, name:'potatoes', calories: 110}, 
+                      {id: 7, name:'celery', calories: 15}, 
+                      {id: 8, name:'carrots', calories: 25}, 
+                      {id: 9, name:'corn', calories: 63}, 
+                      {id: 10, name:'broccoli', calories: 50},];
+    return(<>
+       <List items={fruits} category="Fruts"/>
+       <List items={vegetables} category="Vegetables"/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <br></br>
         <Button/>
         <Header/>
         <Student name="Bob" age={30} isStudent={true}/>
@@ -22,7 +37,8 @@ function App () {
         <Food/>
         <Footer/>
         <UserGreeting isLoggedIn={true} username="Sabina" />
-      </>
+       </>
+     
     );
 }
 
---
+++ src/Components/List/List.tsx
@@ -0,0 +1,23 @@
+function List (props) {
+    const category = props.category;
+    const itemList = props.items;
+    const listitems = itemList.map(item => <li key={item.name}>
+                                            {item.name}: &nbsp;
+                                           <b>{item.calories}</b></li>);
+    return (<>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listitems}</ol>
+            </>);
+}
+export default List;
+
+
+
+
+
+    //const lowCalFruits = fruits.filter(fruit => fruit.calories <= 100);
+    //const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+//fruits.sort((a, b) => a.name.localeCompare(b.name)); sort by nameproperty(Alphabetical)
+//fruits.sort((a, b) => b.name.localeCompare(a.name)); Omvänd sortering på alfabetiskordning
+// fruits.sort((a, b) => a.calories- b.calories);  sort by calories property
+// fruits.sort((a, b) => b.calories- a.calories);  Omvänt sortering på kalorier
